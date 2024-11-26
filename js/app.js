1156bdd6dc8dc2809b39b5754325b60612a9b89a
awmiri <talasaz1382@gmail.com>
2024-11-26 14:33:15 -0800

    create => nav menu and add js code for left style

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 let $ = document;
 
 const getBtn = $.querySelector('.nav__btn');
+const getNav = $.querySelector('.nav-menu')
 let addClass = false ;
+
 getBtn.addEventListener('click' , function() {
     if (addClass) {
-        
         getBtn.classList.remove('nav__btn--open');
+        getNav.style.left = '-25rem'
         addClass = false;
     }else{
         getBtn.classList.add('nav__btn--open');
+        getNav.style.left = '0rem'
         addClass = true;
-
     }
 });
