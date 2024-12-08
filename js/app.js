fba11cc8fcb93d299f2162cc391b8c8da58d472b
awmiri <talasaz1382@gmail.com>
2024-12-08 17:06:06 +0330

    add click object on heart

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let $ = document;
 const getBtn = $.querySelector('.nav__btn');
 const getNav = $.querySelector('.nav-menu');
 const getHeartIcon = $.getElementsByClassName('heart__svg');
-const getHeartContainer = $.querySelector('.destination--icon');
+const getHeartContainer = $.querySelectorAll('.destination--icon');
 const pageLoader = $.querySelector('.loading_page');
 let addClass = false ;
 let hasHeartClass = false;
@@ -19,17 +19,23 @@ getBtn.addEventListener('click' , function() {
         addClass = true;
     }
 });
-getHeartContainer.addEventListener("click" ,function () { 
-    getHeartIcon.forEach( function (item) {
-        if (hasHeartClass) {
-            item.classList.remove('heart__svg-active');
-            hasHeartClass = false;
-        }else{
-            item.classList.add('heart__svg-active')
-            hasHeartClass = true;
-        }     
-    }); 
-})
+
+getHeartContainer.forEach(function(heart) {
+    heart.addEventListener("click", function () {
+        for (let i = 0; i <1; i++) {
+            let heartIcon = this.querySelector('.heart__svg'); 
+        
+        let isActiveIcon = heartIcon.classList.contains('heart__svg-active'); 
+
+        if (isActiveIcon) {
+            heartIcon.classList.remove('heart__svg-active');
+        } else {
+            heartIcon.classList.add('heart__svg-active');
+        }
+            
+        }
+    });
+});
 window.addEventListener('load' , function () {
     pageLoader.classList.add('loadering__page--diactive')
 })
